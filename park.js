987f62bdca185e5954be7e29351debb33629f7d7
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-03 20:47:12 +0100

    adds method and test for filtering by number of offspring

--- park.js
+++ park.js
@@ -6,5 +6,24 @@ Park.prototype.addDinosaur = function (dinosaur) {
   this.enclosure.push(dinosaur);
 };
 
+// Park.prototype.removeByType = function (type) {
+//   for(let dinosaur of this.enclosure) {
+//     if(dinosaur.type === type) {
+//       let indexToRemove = this.enclosure.indexOf(dinosaur);
+//     }
+//     else {
+//       indexToRemove = -1;
+//     }
+//   }
+//   if(indexToRemove > -1) {
+//     this.enclosure.splice(indexToRemove, 1);
+//   }
+// };
+
+Park.prototype.twoPlusOffSpring = function (min) {
+  const result = this.enclosure.filter(dinosaur => dinosaur.offspring > min);
+  return result;
+};
+
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -6,6 +6,8 @@ describe("Park", function() {
   beforeEach(function() {
     park = new Park();
     dinosaur = new Dinosaur("Velociraptor", 3);
+    dinosaur2 = new Dinosaur("T-Rex", 1);
+    dinosaur3 = new Dinosaur("Triceratops", 2);
   })
 
   it('park enclosure is empty', function() {
@@ -15,5 +17,23 @@ describe("Park", function() {
   it('add dinosaur', function() {
     park.addDinosaur(dinosaur);
     assert.strictEqual(park.enclosure.length, 1);
+  })
+
+  // it('remove dinosaurs by type', function() {
+  //   park.addDinosaur(dinosaur);
+  //   park.addDinosaur(dinosaur2);
+  //   park.addDinosaur(dinosaur3);
+  //   assert.strictEqual(park.enclosure.includes(dinosaur), true);
+  //   assert.strictEqual(park.enclosure.length, 3);
+  //   park.removeByType("Velociraptor");
+  //   assert.strictEqual(park.enclosure.includes(dinosaur), false);
+  //   assert.strictEqual(park.enclosure.length, 2);
+  // })
+
+  it('get all with offspring of more than 2', function() {
+    park.addDinosaur(dinosaur);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    assert.strictEqual(park.twoPlusOffSpring(2).length, 1);
   })
 })
