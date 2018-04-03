5c76d7ec08424941572ee5b31aa198f11e4c71f6
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-03 19:54:57 +0100

    adds method to add dinosaur, and passing test

--- park.js
+++ park.js
@@ -2,5 +2,9 @@ var Park = function() {
   this.enclosure = [];
 }
 
+Park.prototype.addDinosaur = function (dinosaur) {
+  this.enclosure.push(dinosaur);
+};
+
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,9 +5,15 @@ var Dinosaur = require("../dinosaur.js");
 describe("Park", function() {
   beforeEach(function() {
     park = new Park();
+    dinosaur = new Dinosaur("Velociraptor", 3);
   })
 
   it('park enclosure is empty', function() {
     assert.strictEqual(park.enclosure.length, 0);
+  })
+
+  it('add dinosaur', function() {
+    park.addDinosaur(dinosaur);
+    assert.strictEqual(park.enclosure.length, 1);
   })
 })
