858675b0b1d261ef63dfc152ffdd54c8d1b03e08
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-03 21:12:53 +0100

    adds method and test to remove dinosaur by type

--- park.js
+++ park.js
@@ -6,19 +6,17 @@ Park.prototype.addDinosaur = function (dinosaur) {
   this.enclosure.push(dinosaur);
 };
 
-// Park.prototype.removeByType = function (type) {
-//   for(let dinosaur of this.enclosure) {
-//     if(dinosaur.type === type) {
-//       let indexToRemove = this.enclosure.indexOf(dinosaur);
-//     }
-//     else {
-//       indexToRemove = -1;
-//     }
-//   }
-//   if(indexToRemove > -1) {
-//     this.enclosure.splice(indexToRemove, 1);
-//   }
-// };
+Park.prototype.removeByType = function (type) {
+  let indexToRemove = -1;
+  for(let dinosaur of this.enclosure) {
+    if(dinosaur.type === type) {
+      indexToRemove = this.enclosure.indexOf(dinosaur);
+    }
+  }
+  if(indexToRemove > -1) {
+    this.enclosure.splice(indexToRemove, 1);
+  }
+};
 
 Park.prototype.twoPlusOffSpring = function (min) {
   const result = this.enclosure.filter(dinosaur => dinosaur.offspring > min);
